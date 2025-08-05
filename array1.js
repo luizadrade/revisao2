0d4d22b7b474a95c86c182468ddc5ef1b3ae8d4f
kz09k <0001179017@senaimgaluno.com.br>
2025-08-05 11:20:05 -0300

    atividadearray

--- array1.js
+++ array1.js
@@ -1,8 +1,13 @@
-let nomefrutas = ['pera','maça',
-    'laranja','manga',['jiló','chuchu','cebola'],
-    ['arroz',['tipo1','tipo2','tipo3']]];
+let nomefrutas = [
+  "pera",
+  "maça",
+  "laranja",
+  "manga",
+  ["jiló", "chuchu", "cebola"],
+  ["arroz", ["tipo1", "tipo2", "tipo3"]],
+];
 
-    nomefrutas[1]='uva';
-    nomefrutas[4][1]='morango';
-    nomefrutas[5][1][1]='tipoB';
-    console.log(nomefrutas);
+nomefrutas[1] = "uva";
+nomefrutas[4][1] = "morango";
+nomefrutas[5][1][1] = "tipoB";
+console.log(nomefrutas);
--- frutas.js
+++ frutas.js
@@ -1,9 +1,8 @@
-let frutas = ['morango',' melancia','laranja','uva'];
-let quantidade = parseFloat('digite o nomes das frutas:');
-for(i=0; i<quantidade; i++)
-{
-    let novafrutas = parseInt('digite nova frutas: '+ (i+1));
-    frutas.push ('nomesfrutas');
-for(i=0; i<=frutas.length; i++)
-    console.log(' A posição do array ' + i + ' eh '+ [frutas[i]])
+let frutas = ["morango", " melancia", "laranja", "uva"];
+let quantidade = parseFloat("digite o nomes das frutas:");
+for (i = 0; i < quantidade; i++) {
+  let novafrutas = parseInt("digite nova frutas: " + (i + 1));
+  frutas.push("nomesfrutas");
+  for (i = 0; i <= frutas.length; i++)
+    console.log(" A posição do array " + i + " eh " + [frutas[i]]);
 }
