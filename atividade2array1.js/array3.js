5acabe500f00547b57ff10c5c4dcbb279fd6c87c
kz09k <0001179017@senaimgaluno.com.br>
2025-08-06 08:17:53 -0300

    teste array

---
+++ atividade2array1.js/array3.js
@@ -0,0 +1,8 @@
+// Lista de frutas
+const frutas = ["Maçã", "Banana", "Laranja", "Uva", "Melancia", "Abacaxi"];
+
+// Exibindo cada fruta no console
+console.log("Lista de frutas:");
+for (let i = 0; i < frutas.length; i++) {
+  console.log(frutas[i]);
+}
